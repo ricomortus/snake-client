f0bd848bb8fd0e32f80131aed2f2d274b1186410
Rico Chow <rico.ck.chow@gmail.com>
2023-10-16 14:09:42 -0400

    Move connect function into a seprate file (client.js) and imports connect function into play.js.

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  //Handle incoming data from server when idled/ kicked out
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 
 console.log("Connecting ...");
