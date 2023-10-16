b86c0734edecd36ddff12019f66732896eed0d1e
Rico Chow <rico.ck.chow@gmail.com>
2023-10-16 14:18:00 -0400

    Add send name event handler on connection to server in client.js.

--- client.js
+++ client.js
@@ -15,6 +15,13 @@ const connect = function () {
     console.log("Server says: ", data);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server.");
+  });
+
+  conn.on("connect", (data) => {
+    conn.write("Name: RC", data);
+  });
   return conn;
 };
 
