62f7a3803019d7411411e7351263069cc619ac02
Rico Chow <rico.ck.chow@gmail.com>
2023-10-17 10:44:34 -0400

    Update input.js with WASD listeners and play.js to pass the connect() obj into setupInput.

--- input.js
+++ input.js
@@ -1,13 +1,27 @@
+// Stores the active TCP connection object.
+let connection;
+
 //setup handleUserInput
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } else if (key === 'm') {
+    connection.write("Say: Hello, Server!");
   }
 };
 
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
