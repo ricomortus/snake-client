be3ccc201bf99dfb914665e884d152b8d113dc0a
Rico Chow <rico.ck.chow@gmail.com>
2023-10-17 10:18:41 -0400

    Create input.js to contain handleUserInput and setupInput for export.

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
 connect();
-
-//setup handleUserInput
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 setupInput();
