b00de3aa9614fb2865a9184932d2135785c850e4
Rico Chow <rico.ck.chow@gmail.com>
2023-10-17 12:28:17 -0400

    Add constants.js to store IP and PORT for export.

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require('./constants')
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,// PORT number here,
+    host: IP,
+    port: PORT,// PORT number here,
   });
 
   // interpret incoming data as text
