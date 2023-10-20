697622161d9fbf82177ae1029f73cf3eb73d7039
Rico Chow <rico.ck.chow@gmail.com>
2023-10-20 12:18:10 -0400

    Update client.js.

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const { IP, PORT } = require('./constants')
+const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function () {
